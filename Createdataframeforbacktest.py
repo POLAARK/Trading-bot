61734df687cf8a0c8a0130e325440b7baa8f6264
Paul B <gamespaul@outlook.fr>
2023-03-11 19:00:28 +0100

    Trading bot

---
+++ Createdataframeforbacktest.py
@@ -0,0 +1,223 @@
+from cProfile import label
+from pickle import TRUE
+from turtle import back
+from attr import NOTHING
+from pandas import *
+import pandas as pd
+import yfinance as yf
+import requests as rq
+from binance import *
+import time as time
+from matplotlib import *
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime as datetime
+from math import *
+from decimal import *
+import Indicateursbacktest as ind
+import os
+
+#On va faire ça avec l'api binance
+#Clefs qui doivent etre passées en privées 
+
+#Pour récupérer des informations a propose de symboles on a besoin des clefs apis binance 
+#Ces dernieres sont à insérer ci dessous 
+api_key = '0W0NnouXJG5kHRuvjm7AcZNOSYxHHPmNWItts8ZUWcIp9aQv4QyCKUa1EbRTE4Iw'
+api_secret = 'NmW0ruph3E8qvg5c9c9ngEgukPVkHKCHYBPE27ZB8UBtD7kvI79JiWQDU7SXbwrF'
+client = Client(api_key, api_secret)
+
+
+def dataframe(filename, timeframe : str, Starttime : int , backtest : bool, pair : str,):
+    """
+    Fonction qui renvoit la data depuis le temps spécifié.
+    Créé une dataframe à partir de différentes données spécifiées
+
+    ---
+    Paramètres
+    ---
+
+    -La timeframe (1m, 5m, 15m, 30m, 1h, 2h, 4h, 1d)
+
+    -Le temps de début en unix à 10 digit (donc milli secondes)
+
+    -Backtest : True si les données doivent etre sauvegardés pour pouvoir travailler dessus ou False si c'est des données à utiliser en live trading
+    
+    -La paire avec "pair" : Toutes les paires disponibles sur binance sont récupérables. Par exemple : BTCUSDT renvoie les différents prix du bitcoin en fonction du prix du dollar.
+
+    ---
+    Remarques
+    ---
+    On n'effectue pas de calculs des indicateurs sur la dataframe 1 minutes car, on n'élabore pas de stratégies sur une timeframe aussi faible (trop volatile) et le temps de calcul des indacateurs est
+    très long
+    """
+        
+    #Ici choix de la timeframe
+    if timeframe == '5m':
+        timeframe =  client.KLINE_INTERVAL_5MINUTE
+    elif timeframe == '1m':
+        timeframe = client.KLINE_INTERVAL_1MINUTE
+    elif timeframe == '15m':
+        timeframe = client.KLINE_INTERVAL_15MINUTE
+    elif timeframe == '1h':
+        timeframe = client.KLINE_INTERVAL_1HOUR
+    elif timeframe == '2h':
+        timeframe = client.KLINE_INTERVAL_2HOUR
+    elif timeframe == '4h':
+        timeframe = client.KLINE_INTERVAL_4HOUR
+    elif timeframe == '1d':
+        timeframe = client.KLINE_INTERVAL_1DAY
+    
+    #création des bougies passées:
+    klines = client.get_historical_klines(pair, timeframe, Starttime)
+    Lopen = []
+    Lhigh = []
+    Llow = []
+    Lclose = []
+    Ldates = []
+    #création des liste pour la base de données
+    for k in klines:
+        Lopen.append(float(k[1])) #Open
+        Lhigh.append(float(k[2])) #High
+        Llow.append(float(k[3])) #Low
+        Lclose.append(float(k[4])) #Close
+        Ldates.append(int(k[0]/1000))
+
+    d = {'date': Ldates, 'open' : Lopen, 'high' : Lhigh, 'low' : Llow, 'price' : Lclose}
+    
+    df = pd.DataFrame(d)
+    dataframe = df.reset_index(drop = True)
+    if timeframe != '1m':
+        ind.PSAR(dataframe)
+        ind.zscore(dataframe, 20, "price")
+        ind.sma(dataframe,20, '20Zscore_price')
+        ind.sma(dataframe, 7, 'price')
+        ind.MACD(dataframe, 'price')
+    
+
+
+    return dataframe
+
+def update():
+    try :
+        nomfichierdata = input('Entrez le nom de fichier EXACT : ')
+        dataframe_update = pd.read_pickle(nomfichierdata)
+    except Exception:
+        print('Le nom du fichier n\'est pas correct vérifiez et re-essayer')
+        return
+    indice = nomfichierdata.find('mois')
+    timeframe = nomfichierdata[indice+4:indice + 6]
+    if timeframe == '15':
+        timeframe = '15m'
+    pair = checkasset(nomfichierdata, timeframe)
+    cutupdate = (dataframe_update.loc[len(dataframe_update)-1, 'date'] + getpasindex(timeframe))
+    startupdate = str((dataframe_update.loc[len(dataframe_update)-1, 'date'] - getstartindex(timeframe))*1000)
+    datatoappend  = dataframe(nomfichierdata, timeframe, startupdate, False,pair)
+    cutindex = datatoappend.index[datatoappend['date'] == cutupdate].tolist()[0]
+    datato = datatoappend.iloc[cutindex:len(datatoappend)-1]
+    dataframenew= pd.concat([dataframe_update,datato])
+    dataframenew = dataframenew.reset_index(drop = True)
+    lengthofdataframe = round((int(time.time()) - dataframe_update.loc[0,'date'])/(3600*24*30),1)
+    nouveaunom = 'dataframe' + str(lengthofdataframe) + nomfichierdata[indice:]
+    dataframenew.to_pickle(nouveaunom)
+
+def checkasset(string : str,timeframe : str):
+    string = string[::-1]
+    timeframe = timeframe[::-1]
+    ind = string.find(timeframe)
+    asset = string[0:ind]
+    return asset[::-1]
+
+
+def getstartindex(timeframe):
+        pas = 0
+        if timeframe == '1m':
+            pas = 200 * 60
+        elif timeframe == '5m':
+            pas =  200 * 300
+        elif timeframe == '15m':
+            pas = 200 * 15 * 60
+        elif timeframe == '1h':
+            pas = 200 * 60 * 60
+        elif timeframe == '2h':
+            pas = 200 * 60 * 120
+        elif timeframe == '4h':
+            pas = 200 * 60 * 60 * 4
+        elif timeframe == '1d':
+            pas = 200 * 60 * 60 * 24
+        return pas
+
+def getpasindex(timeframe):
+        pas = 0
+        if timeframe == '1m':
+            pas = 60
+        elif timeframe == '5m':
+            pas =  300
+        elif timeframe == '15m':
+            pas = 15 * 60
+        elif timeframe == '1h':
+            pas = 60 * 60
+        elif timeframe == '2h':
+            pas = 60 * 120
+        elif timeframe == '4h':
+            pas = 60 * 60 * 4
+        elif timeframe == '1d':
+            pas = 60 * 60 * 24
+        return pas
+"""
+Ci dessus est le code juste pour permettre à l'utilisateur de rentrer les données pour créer la dataframe qui est intéréssante à backtester
+Il suffit de répondre aux questions possées lors du lancement du code 
+Il y a une gestion des erreurs dans le cas ou les données proposées ne correspondent pas aux données attendus
+"""
+
+def delete_column():
+    try :
+        nomfichierdata = input('Entrez le nom de fichier EXACT : ')
+        df = pd.read_pickle(nomfichierdata)
+    except Exception:
+        print('Le nom du fichier n\'est pas correct vérifiez et re-essayer')
+        return
+    print('Columns list : ')
+    print(df.columns)
+    column = input('Input a column to delete : ')
+    df.drop(column, axis=1, inplace=True)
+    print(' Column has been deleted')
+    df.to_pickle(nomfichierdata)
+
+
+def __main__():
+    backtest = input("Voulez vous download une dataframe? (True or False) ")
+    if backtest == 'True':
+        timeframe = input("Choisissez une timeframe : (1m, 5m, 15m, 30m, 1h, 2h, 4h, 1d) ")
+        timestart = input("Choisissez un temps unix en milliseconde pour démarer la création de dataframe : ")
+        try:
+            timestart = int(timestart)
+            test1 = timestart/(10**12)
+            if int(test1) !=1:
+                timestart = 1629756000000 #valeur par défault
+                print('timestart value is not usuable default value 1629756000000 has been used')
+            test2 = timestart - ceil(timestart/10)*10
+            if int(test2) != 0:
+                timestart = 1629756000000 #valeur par défault
+                print('timestart value is not usuable default value 1629756000000 has been used')
+        except:
+            timestart = 1629756000000
+            print('timestart value is not usuable default value 1629756000000 has been used')
+        paire = input('Choisissez la paire pour la base de données (Ex: BTCUSDT) : ')
+        lengthofdataframe = round((int(time.time()) - timestart/1000)/(3600*24*30),1)
+        name = "dataframe" + str(lengthofdataframe) + 'mois' + timeframe + paire
+        dataframe1 = dataframe(name, timeframe, timestart, True, paire)
+        dataframe1.to_pickle(name)
+    else:
+        update1 = input('Voulez vous mettre à jour une dataframe ? (True ou False) ')
+        if update1 == 'True':
+            update()
+        else : 
+            modify = input('Voulez vous retirer une colonne d\'une dataframe ? (True ou False) ')
+            if modify:
+                delete_column()
+
+__main__()
+
+"""
+# TESTS NON UTILES
+"""
---
+++ basebacktest.py
@@ -0,0 +1,211 @@
+from pickle import TRUE
+from pandas import *
+import pandas as pd
+from binance import *
+import asyncio
+from matplotlib import *
+import matplotlib.pyplot as plt
+import matplotlib
+from datetime import datetime
+from math import *
+import automatisationbacktest as aut
+import Indicateursbacktest as ind
+
+def Selecttime(dataframe,startime, endtime):
+    Lx = []
+    for k in range(startime,endtime+1):
+        
+        Lx.append(dataframe.loc[k,'date'])
+    return Lx
+
+#importation des dataframe pour le backtest:
+input_dataframe = "./dataframe17.9mois15mBTCUSDT"
+dataframe6mois5m = pd.read_pickle(input_dataframe)
+dataframe6mois1m = pd.read_pickle("./dataframe17.9mois1mBTCUSDT")
+
+for k in range(len(input_dataframe)):
+    if input_dataframe[k:k+4] == 'mois' :
+        if input_dataframe[k+6] == 'm' : 
+            timeframe = input_dataframe[k+4:k+7]
+        else:
+            timeframe = input_dataframe[k+4:k+6]
+# print(list(dataframe6mois5m.columns))
+# Initialisation de liste pour différentes représenations graphiques
+LCapital = []
+Lprice = []
+Lindex = []
+async def main(filename,backtest,start,end):
+    #Valeurs de TP/SL pour la stratégie (a adapter en fonction de la stratégie à bakctester)
+    TP = 0.01
+    SL = 0.005
+    typetrade = 'long'
+    #On veut avoir le nombre de trade
+    nombreDeTrade = 0
+    #Variable pour utile dans certaines stratégies
+    Bornes = 0
+    #Variable de teste pour savoir si un trade est en cours ou pas
+    enCours = False
+    #On initialise le capital de départ à 1000
+    Capital = 1000
+    longpossibility = False
+    shortpossibility = False
+
+    if backtest == 'True':
+
+        Gainmoyentrade = 0 
+        Winrate = 0
+        k = start
+        #On instancie un objet backtest avec les données acutelles, elles seront modifiées à chaques entré dans un trade
+        back1 = aut.backtest(dataframe6mois5m, dataframe6mois1m, filename,\
+        TP, SL, k, end , enCours, Capital, nombreDeTrade, Winrate, Gainmoyentrade, Lindex ,Lprice, LCapital)
+        ZSCOREEMA = dataframe6mois5m['20Zscore_price'].ewm(span = 14 , adjust = False).mean()
+        #On parcourt toute la dataframe
+        while k < end:
+            """"
+            On parcourt toute la dataframe
+
+            A chaque étape on change les variables essentiels dans le backtesting (puisqu'elles sont différentes pour tout k). 
+            Ici on peut initialiser des conditions pour ouvrir des shorts et des longues, enfaite on va définir la stratégie à backtester
+            
+            Toutes les valeurs numériques sont changeable, elles sont choisis car c'est celles qui donnent les meilleurs rendement mais  
+            il peut en exister des meilleurs
+
+            /!\ Si du code est commenté c'est qu'il est souvent utile mais pas dans le dernier cas testé. 
+            """
+            enCours = False
+            #Ici on met à jour les informations importantes dans le backtest à chaque itération
+            back1.updateL(back1.Capital,datetime.utcfromtimestamp(dataframe6mois5m.loc[int(k),'date']).strftime('%Y-%m-%d %H:%M:%S'),dataframe6mois5m.loc[int(k),'price'])
+
+            
+            """ Code commenté utile dans certaines stratégies"""
+            if dataframe6mois5m.loc[int(k),'20Zscore_price'] >= 3: #Ici code pour startégie zscore
+                shortpossibility = True
+            elif dataframe6mois5m.loc[int(k),'20Zscore_price'] <= 0:
+                shortpossibility = False
+            if dataframe6mois5m.loc[int(k),'20Zscore_price'] <= -3 :
+                longpossibility = True
+            elif dataframe6mois5m.loc[int(k),'20Zscore_price'] >= 0:
+                longpossibility = False
+            """ Fin Code commenté utile dans certaiens stratégies"""
+
+            #On définit les entrées dans un trade (entrée long =/= de entrée short)
+            
+            Entryconditionshort = shortpossibility and dataframe6mois5m.loc[int(k-1), '20Zscore_price'] > dataframe6mois5m.loc[int(k), '20Zscore_price'] and ZSCOREEMA[k] > dataframe6mois5m.loc[int(k), '20Zscore_price']
+            Entryconditionlong = False #longpossibility and dataframe6mois5m.loc[int(k-1), '20Zscore_price'] < dataframe6mois5m.loc[int(k), '20Zscore_price'] and ZSCOREEMA[k] < dataframe6mois5m.loc[int(k), '20Zscore_price'] and dataframe6mois5m.loc[k,'PSARdir'] == 'bull'
+            
+            # longpossibility and dataframe6mois5m.loc[int(k-1), '20Zscore_price'] < dataframe6mois5m.loc[int(k), '20Zscore_price'] and dataframe6mois5m.loc[int(k), '20SMA_20Zscore_price'] < dataframe6mois5m.loc[int(k), '20Zscore_price']
+            # shortpossibility and dataframe6mois5m.loc[int(k-1), '20Zscore_price'] > dataframe6mois5m.loc[int(k), '20Zscore_price'] and dataframe6mois5m.loc[int(k), '20SMA_20Zscore_price'] > dataframe6mois5m.loc[int(k), '20Zscore_price']
+            #Si les conditions précédentes sont réalisées alors on rentre dans un trade et on entre aussi dans la partie automatique du backetesting
+            #la base de donnée va etre parcouru à la recherche d'une condition de sortie
+            #L'inconvenient c'est que les conditions de sorties outre TP et SL doivent etre mentionner dans automatisaiont_backtest() avant le début du bakctest
+            #
+            if Entryconditionshort and not Entryconditionlong:
+                typetrade = 'short'
+                enCours = True
+                #Il faut ici changer la timeframe en fonction de sur quelle timeframe est la stratégie principale. 
+                back1.automatisation_backtest(TP , SL, typetrade, Entryconditionshort, Entryconditionlong, k, enCours, timeframe, Lindex, Lprice, LCapital, True)
+                k = back1.indice
+                shortpossibility = False
+            
+            if Entryconditionlong and not Entryconditionshort:
+                typetrade = 'long'
+                enCours = True
+                back1.automatisation_backtest(TP , SL, typetrade, Entryconditionlong, Entryconditionlong, k, enCours, timeframe, Lindex, Lprice, LCapital)
+                k = back1.indice
+                longpossibility = False
+            k+=1
+                
+#sauvegarde de variables utiles pour juger de la viabilité d'une stratégie
+    try:
+        Winrate = (back1.Winrate / back1.nombreDeTrade) * 100
+    except :
+        Winrate = 0
+
+    print("nombre de trade: " + str(back1.nombreDeTrade))
+    try:
+        print("gain moyen par trade: " + str(back1.Gainmoyentrade/back1.nombreDeTrade))
+    except:
+        print("gain moyen par trade: 0")
+    print("Winrate: " + str(Winrate) + "%")
+    # print(dataframe6mois5m.iloc[debut:fin,13].tolist())
+    # print(Lindex)
+    x = matplotlib.dates.date2num(Lindex)
+    formater = matplotlib.dates.DateFormatter('%Y-%m-%d')
+
+    figure = plt.figure()
+    axes1 = figure.add_subplot(2, 1, 1)
+    axes1.xaxis.set_major_formatter(formater)
+    axes1.set_xlabel('Time')
+    axes1.set_ylabel('Capital Evolution')
+    axes1.tick_params(labelrotation=15)
+    axes1.plot(x, back1.LCapital)
+
+    axes2 = figure.add_subplot(2, 1, 2)
+    axes2.xaxis.set_major_formatter(formater)
+    axes2.set_xlabel('Time')
+    axes2.set_ylabel('')
+    axes2.tick_params(labelrotation=15)
+    axes2.plot(x, Lprice)
+
+    # axes3 = figure.add_subplot(3, 1, 3)
+    # axes3.xaxis.set_major_formatter(formater)
+    # axes3.set_xlabel('Time')
+    # axes3.set_ylabel('Zscore')
+    # axes3.tick_params(labelrotation=15)
+    # axes3.plot(x, dataframe6mois5m.iloc[debut:fin,13].tolist())
+    
+    figure.tight_layout()
+    plt.show()
+    
+
+a = input("Entrez le nom_du_fichier : ")
+if a == '':
+    print('Fichier par défaut utilisé : essai.json')
+    a = "essai"
+a = a + '.json'
+datei = datetime.utcfromtimestamp(dataframe6mois5m.loc[0,'date']).strftime('%Y-%m-%d')
+datef = datetime.utcfromtimestamp(dataframe6mois5m.loc[len(dataframe6mois5m)-1,'date']).strftime('%Y-%m-%d')
+debut = input("Faire le choix de la date de début (de " + datei + " à " +  datef + " ) : ")
+fin = input("Faire le choix de la date de fin, doit être supérieur à la date de début et inférieur à " + datef + " : ")
+if debut == '' and fin == '':
+    
+    debut = '2021-08-25'
+    fin = '2023-02-09'
+    print('Dates par défauts utilisées : debut =  ' + debut + ', fin =  ' + fin)
+debut = int(datetime.timestamp(datetime.strptime(debut + ' 00:00:00', '%Y-%m-%d %H:%M:%S')))
+fin = int(datetime.timestamp(datetime.strptime(fin + ' 00:00:00', '%Y-%m-%d %H:%M:%S')))
+
+debut = dataframe6mois5m.index[dataframe6mois5m['date'] == debut].tolist()[0]
+fin = dataframe6mois5m.index[dataframe6mois5m['date'] == fin].tolist()[0]
+asyncio.run(main(a ,'True',debut,fin))
+
+
+""""
+
+Ici on sauvegarde du code utile pour le backteste d'ancienne stratégie
+On a donc des exemples de code intéréssant et réutilisable
+
+"""
+#
+#cas du bot stratégie 1 
+#
+# if dataframe6mois5m.loc[int(k),'20Zscore_price'] >= 3:
+#    shortpossibility = True
+#if dataframe6mois5m.loc[int(k),'20Zscore_price'] <= -3:
+#    longpossibility = True
+#Entryconditionshort = shortpossibility and dataframe6mois5m.loc[int(k),'20Zscore_price'] < dataframe6mois5m.loc[int(k) - 1, '20Zscore_price'] and dataframe6mois5m.loc[int(k),'20Zscore_price'] < dataframe6mois5m.loc[int(k), '20SMA_20Zscore_price']
+#Entryconditionlong = longpossibility and dataframe6mois5m.loc[int(k),'20Zscore_price'] > dataframe6mois5m.loc[int(k) - 1, '20Zscore_price'] and dataframe6mois5m.loc[int(k),'20Zscore_price'] > dataframe6mois5m.loc[int(k), '20SMA_20Zscore_price']
+
+#
+#Cas du bot stratégie 2 
+#
+# if dataframe6mois5m.loc[int(k),'20Zscore_price'] >= 3:
+#     shortpossibility = True
+# elif dataframe6mois5m.loc[int(k),'20Zscore_price'] <= 1:
+#     shortpossibility = False
+# if dataframe6mois5m.loc[int(k),'20Zscore_price'] <= -3 :
+#     longpossibility = True
+# elif dataframe6mois5m.loc[int(k),'20Zscore_price'] >= -1:
+#     longpossibility = False
+# Entryconditionshort = dataframe6mois5m.loc[int(k),'7SMA_price'] >= dataframe6mois5m.loc[int(k),'20SMA_price'] and shortpossibility and dataframe6mois5m.loc[int(k),'20Zscore_price'] < dataframe6mois5m.loc[int(k) - 1,'20Zscore_price']
+# Entryconditionlong = dataframe6mois5m.loc[int(k),'7SMA_price'] <= dataframe6mois5m.loc[int(k),'20SMA_price'] and longpossibility and dataframe6mois5m.loc[int(k),'20Zscore_price'] > dataframe6mois5m.loc[int(k) - 1,'20Zscore_price']
